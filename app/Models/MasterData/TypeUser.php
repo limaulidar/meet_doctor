4a2fcb3e298cb1887eb8870652cb254dfb52358f
li <68934622+limaulidar@users.noreply.github.com>
2022-05-04 12:08:46 +0700

    create model

---
+++ app/Models/MasterData/TypeUser.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models\MasterData;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class TypeUser extends Model
+{
+    // use HasFactory;
+    use SoftDeletes;
+
+    // declare table
+    public $table = 'type_user';
+
+    // this field must type date yyyy-mm-dd hh:mm:ss
+    protected $dates = [
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    // declare fillable
+    protected $fillable = [
+        'name',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+    ];
+
+    //ONE TO many
+    public function detail_user()
+    {
+        //2 parameter (path, field foreign key)
+        return $this->hasMany('App\Models\ManagementAccess\DetailUser','tyepe_user_id');
+    }
+
+
+}
