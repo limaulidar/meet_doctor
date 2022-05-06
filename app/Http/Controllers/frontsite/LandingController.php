d8fc71403e3bab2cfef0f21d90259f06114526ee
li <68934622+limaulidar@users.noreply.github.com>
2022-05-06 21:22:12 +0700

    update route 30%

--- app/Http/Controllers/frontsite/LandingController.php
+++ app/Http/Controllers/frontsite/LandingController.php
@@ -28,17 +28,6 @@ class LandingController extends Controller
 {
 
     /**
-     * create a new controller instance
-     * 
-     * @return void
-     */
-
-    public function __construct ()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
